bf1de3ed57d8e39cb2580a6c0a1ceae0e444654d
Jude Anthony <judeanth@amazon.com>
2019-01-01 16:56:22 -0800

    Alphabetic tag cloud in sidebar

--- make_tags.py
+++ make_tags.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 import re
 import yaml
-
+from collections import Counter
 import glob
 import os
+import math
 
 post_dir = '_posts/'
 tag_dir = '_tagged/'
+tagcloud_filename = '_includes/tag_cloud.html'
 
 filenames = glob.glob(post_dir + '*html')
 
@@ -17,6 +19,7 @@ for filename in filenames:
     post_info = next(docs)
     total_tags.extend(post_info['tags'])
     f.close()
+freqs = Counter(total_tags)
 total_tags = set(total_tags)
 
 existing_tagfiles = glob.glob(tag_dir + '*.md')
@@ -38,4 +41,21 @@ permalink: /progress/tag/{slug}
 '''.format(tag=tag, slug=slug)
     f.write(front_matter)
     f.close()
-print("Tags generated, count", total_tags.__len__())
+print("Tag pages generated, count", total_tags.__len__())
+
+f = open(tagcloud_filename, 'w')
+f.write('<div class="tag-cloud">')
+maxcount = max(freqs.values())
+first = True;
+for tag, postcount in sorted(freqs.iteritems()):
+  factor = math.log(maxcount)
+  size = 100 * (math.log(postcount) / factor) + 100
+  slug = re.sub('[-\s]+', '-', tag).strip().lower()
+  if (first):
+    first = not first;
+  else:
+    f.write(' &bull; ');
+  f.write('<span style="font-size:{size}%"><a href="/progress/tag/{slug}">{tag}({count})</a></span>'
+    .format(tag=tag, size=size, slug=slug, count=postcount))
+f.write('</div>')
+print("Generated tag cloud")
