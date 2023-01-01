f494130453764bd7b958781126a0cf794b569245
Jude Anthony <judeanth@amazon.com>
2023-01-01 11:06:18 -0800

    Use move history instead of grid history

--- src/App.js
+++ src/App.js
@@ -5,18 +5,46 @@ import Board from './Board.js';
 class App extends React.Component {
     state = {
         size: 9,
-        history: [Array(9 * 9).fill(0)],
+        history: [],
         step: 0,
         icons: 'ringer-monochrome',
         depth: 2,
     };
 
-    // Increment all the cells in a ring around (x, y)
-    ring(x, y) {
-        let size = this.state.size;
+    render() {
         let step = this.state.step;
         let history = this.state.history.slice(0, step + 1);
-        let current = history[step].slice();
+        let grid = this.gridFrom(history);
+        return (
+            <div className="App">
+              <header className="App-header">
+                Ringer
+              </header>
+              <section className="App-content">
+                <Board size={this.state.size}
+                  cells={grid}
+                  icons={this.state.icons}
+                  onClick={(x, y) => this.makeMove(x, y)}
+                />
+              </section>
+            </div>
+        );
+    };
+
+    makeMove(x, y) {
+        let step = this.state.step + 1;
+        let history = this.state.history.slice(0, step).concat([[x, y]]);
+        let current = this.gridFrom(history);
+        this.setState({
+            history: history,
+            step: step,
+        });
+    }
+
+    // Increment all the cells in a ring around (x, y) *mutating* the given grid
+    ring(x, y, grid) {
+        let size = this.state.size;
+        let depth = this.state.depth;
         const neighbors = [
             [-1, -1], [0, -1], [1, -1],
             [-1, 0], [1, 0],
@@ -26,33 +54,18 @@ class App extends React.Component {
             const ringX = (x + dx + size) % size;
             const ringY = (y + dy + size) % size;
             const index = ringY * size + ringX;
-            current[index] = (current[index] + 1) % this.state.depth;
+            grid[index] = (grid[index] + 1) % depth;
         }
-        history.push(current);
-        this.setState({
-            history: history,
-            step: step + 1,
-        });
     }
 
-    render() {
-        let history = this.state.history.slice();
-        let step = this.state.step;
-        let current = history[step].slice();
-        return (
-            <div className="App">
-              <header className="App-header">
-                Ringer
-              </header>
-              <section className="App-content">
-                <Board size={this.state.size}
-                  cells={current}
-                  icons={this.state.icons}
-                  onClick={(x, y) => this.ring(x, y)}/>
-              </section>
-            </div>
-        );
-    };
+    gridFrom(moves) {
+        let size = this.state.size;
+        let grid = new Array(size * size).fill(0);
+        for (var [x, y] of moves) {
+            this.ring(x, y, grid);  // Uses state: size and depth
+        }
+        return grid;
+    }
 }
 
 export default App;
