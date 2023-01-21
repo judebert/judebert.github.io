27b7c2a62c9782294a05604a5bb2812c8b7cfae6
Jude Anthony <judeanth@amazon.com>
2023-01-20 17:48:25 -0800

    Ringer v0.1.8: refactor logic into classes

--- apps/ringer/src/Ringer.js
+++ apps/ringer/src/Ringer.js
@@ -1,3 +1,4 @@
+import seedrandom from 'seedrandom';
 
 class Ringer {
     constructor(size, depth) {
@@ -5,17 +6,20 @@ class Ringer {
         if (depth === undefined) depth = 2;
         this.size = size;
         this.depth = depth;
-        this.boardNum = -1;
+        this.boardNum = 0;
+        this.goal = 0;
         this.start = [];
-        this.moves = [];
+        this.startGrid = this.gridFrom(this.start);
+        this.startDepths = this.depthFrom(this.start);
         this.info = `Tap any cell to flip the ring of cells around it.
           The whole board is a ring, too: flipping a cell outside an edge flips the cell on the opposite edge!
           Can you get all the cells to match?`;
     }
 
     // Helper function converting an [x, y] array to a Ringer cell index
-    _asIndex(point) {
-        return point[1] * this.size + point[0];
+    _asIndex([x, y]) {
+        let val = y * this.size + x;
+        return val;
     }
 
     // Helper function showing how far a cell with a click-depth is from a given target,
@@ -40,18 +44,69 @@ class Ringer {
         return (target - clicks + this.depth) % this.depth;
     }
 
-    ring(cell) {
-        if (cell instanceof Array) {
-            cell = this._asIndex(cell);
+    // Increment all the cells in a ring around (x, y) *mutating* the given grid
+    _ticNeighbors(x, y, grid) {
+        const neighbors = [
+            [-1, -1], [0, -1], [1, -1],
+            [-1, 0], [1, 0],
+            [-1, 1], [0, 1], [1, 1],
+        ];
+        for (var [dx, dy] of neighbors) {
+            const ringX = (x + dx + this.size) % this.size;
+            const ringY = (y + dy + this.size) % this.size;
+            const index = this._asIndex([ringX, ringY]);
+            grid[index] = (grid[index] + 1) % this.depth;
         }
-        this.moves.push(cell);
+    }
+
+    // Returns true if the moves solve the board.
+    // Returns true if every cell in the board is the same color after applying the moves.
+    isSolvedBy(moves) {
+        let grid = this.gridFrom(moves);
+        let response = grid.every((cell) => cell === grid[0]);
+        return response;
+    }
+
+    // Clears this board, then adds clicks until it can be solved in `times` clicks.
+    // Saves the [x,y] start moves in this.start
+    shuffle(seed, times) {
+        let rng = seedrandom(seed);
+        // How many clicks will it take to solve the board right now?
+        this.start = [];
+        let board = this.depthFrom([]);
+        let clicks = 0; // solveDistance is always 0 on an empty board... right?
+        // Shuffle until we reach the right number of clicks
+        for (var i = 0; i < 1000 && clicks < times; i++) {
+            const index = Math.floor(rng() * board.length);
+            // Don't *solve* a cell; only make it deeper
+            if (board[index] !== 1) {
+                board[index] = (board[index] + this.depth - 1) % this.depth;
+                clicks++;
+            }
+        }
+        this.goal = board.reduce((sum, cellDepth) => sum + ((this.depth - cellDepth) % this.depth), 0);
+        console.log(`Shuffled to ${clicks} moves / goal ${this.goal} in ${i} tries`);
+        // Turn that into a list of moves (order doesn't matter!)
+        // TODO: Use a list of indexes, c'mon
+        this.start = board.flatMap((cellDepth, index) =>
+            Array(cellDepth).fill([index % this.size, Math.floor(index / this.size)]));
+        this.startDepths = board;
+    }
+
+    // Returns displayed board, where all the squares *around* the `moves` have been flipped.
+    gridFrom(moves) {
+        let grid = new Array(this.size * this.size).fill(0);
+        for (var [x, y] of this.start.concat(moves)) {
+            this._ticNeighbors(x, y, grid, this.size, this.depth);
+        }
+        return grid;
     }
 
     // Returns a "hit map" of the board after applying the moves.
     // Indicates how many clicks were made at each cell, looping back to 0 after reaching depth.
     depthFrom(moves) {
         let depths = new Array(this.size * this.size).fill(0);
-        for (var move of moves) {
+        for (var move of this.start.concat(moves)) {
             let index = this._asIndex(move);
             depths[index]++;
             depths[index] %= this.depth;
@@ -113,7 +168,7 @@ class Ringer {
         this.boardNum = data.boardNum;
         this.start = data.start;
         this.info = data.info;
-        this.depths = this.depthFrom(this.start);
+        this.depths = this.depthFrom([]);
     }
 }
 
